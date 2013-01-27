ff5397ead2c9555c30723881789dae791b5d7241
Mike Moore <mickety.mike@gmail.com>
2013-01-27 13:58:37 -0600

    DB_Backend: changing default server address to use mb-creations.net

--- dbConfig.py
+++ dbConfig.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 class dbConfig:
     def __init__(self):
-      self.ip = 'localhost'
+      self.ip = 'http://www.mb-creations.net'
       self.port  = 27017
       self.connection = MongoClient(self.ip, 27017)
       self.table = self.connection.test_db.students
