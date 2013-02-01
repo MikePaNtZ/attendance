f81113359c918f8024666719665668eaa704edd1
Mike Moore <mickety.mike@gmail.com>
2013-01-31 21:15:21 -0600

    Attempting a fix to dbConfig.py. Removing www. from the db server hostame seemed to work on a LAN test.

--- dbConfig.py
+++ dbConfig.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 class dbConfig:
     def __init__(self):
-      self.ip = 'http://www.mb-creations.net'
+      self.ip = 'mb-creations.net'
       self.port  = 27017
-      self.connection = MongoClient(self.ip, 27017)
+      self.connection = MongoClient(self.ip, self.port)
       self.table = self.connection.test_db.students
